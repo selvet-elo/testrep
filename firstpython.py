36d8a32bfadd29c0d47a8a8776d0e874fdf9cf9f
selvet-elo <selvet.elif@gmail.com>
2024-09-08 19:55:31 +0300

    python_exercises

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,9 @@
 #learning pyhton
 print("Selvet is learning")
+
+print("Now there is some math")
+
+a=2
+b=8
+c=a+b
+print(c)
---
+++ learning_python.py
@@ -0,0 +1,10 @@
+
+annie = 1996
+jane = 1999
+
+if annie %4 == 0:
+    print("annie was born in the leap year")
+elif jane %4 == 0:
+    print("jane was born in the leap year")
+else:
+    print("none of'em were born in the leap year")
